820964a58696b348635cb84c7b4ed4537d2b5c7a
CloverJoy <chris.ang.dev@gmail.com>
2021-01-26 22:12:04 -0800

    Day 3

---
+++ day3/operators.py
@@ -0,0 +1,21 @@
+age = 24
+height = 169.5
+complex_number = 12j
+print(type(complex_number))
+
+# base = int(input('Enter base: '))
+# height = int(input('Enter height: '))12
+# area = 0.5 * base * height
+# print('The area of the triangle is ', area)
+print(not(len('python') == len('jargon')))
+print('on' in 'python' and 'on' in 'jargon')
+print(not('on' in 'dragon' and 'on' in 'python'))
+print('jargon' in 'I hope this course is not full of jargon')
+print(4 % 2 is 0)
+print(7 // 3 == int(2.7))
+print(type('10') is type(10))
+print(int(9.8) == 10)
+
+hours = int(input('enter hours: '))
+rate = int(input('enter rate: '))
+print('Your weekly earning is ', hours*rate)
