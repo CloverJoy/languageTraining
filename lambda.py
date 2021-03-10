66dd397e34064e4947a9d4eb7362999ab3889caa
CloverJoy <chris.ang.dev@gmail.com>
2021-03-09 22:59:18 -0800

    Lambda function  list comperhension

---
+++ lambda.py
@@ -0,0 +1,30 @@
+# lambda function / anon func / HOF
+items = [
+    ("prod1", 1033),
+    ("prod2", 112),
+    ("prod3", 31),
+]
+
+items.sort(key=lambda item: item[1])
+print(items)
+
+# map
+
+# di list in biar karena map return iterable object map
+prices = list(map(lambda price: price[1], items))
+print(prices)
+
+filtered = list(filter(lambda price: price[1] > 40, items))
+print(list(map(lambda item: item[1], filtered)))
+
+# map but with list comperhension
+# WOOOOOOW PYTHON
+# [expression for item in items]
+prices = [item[1] for item in items]
+print(prices)
+
+filtered = [item[1] for item in items if item[1] > 40]
+print(filtered)
+
+zipped = list(zip(prices, filtered))
+print(zipped)
