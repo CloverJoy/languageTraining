8900083208100455c8ba9b9eb45961ab6945892a
Christoffel Angga <Chris.ang.dev@gmail.com>
2021-03-24 16:06:57 -0700

    Added permission

--- solidity/SharedWallet/SharedWallet.sol
+++ solidity/SharedWallet/SharedWallet.sol
@@ -4,7 +4,18 @@ pragma solidity ^0.8.3;
 
 contract SharedWallet {
 
-    function withdrawMoney(address payable _to, uint _amount) public {
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Now allowerd, only owner!!");
+        _;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
         _to.transfer(_amount);
     }
 
